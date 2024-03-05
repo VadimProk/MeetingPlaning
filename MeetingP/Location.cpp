266c598e81ad670c1b7bf71e01ed3e42eeced3f4
VadimProk <provad40@gmail.com>
2024-03-05 12:15:12 +0200

    Add class Location

---
+++ MeetingP/Location.cpp
@@ -0,0 +1,33 @@
+#include "Location.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+Location::Location(std::string new_country, std::string new_city, std::string new_street) :
+	country{ new_country }, city{ new_city }, street{ new_street } {}
+
+Location::~Location() {}
+
+string Location::getCountry()
+{
+	return country;
+}
+
+string Location::getCity()
+{
+	return city;
+}
+
+string Location::getStreet()
+{
+	return street;
+}
+
+void Location::display()
+{
+	cout << "Країна : " << country << endl;
+	cout << "Місто : " << city << endl;
+	cout << "Вулиця : " << street << endl << endl;
+}
+
---
+++ MeetingP/Location.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Location {
+private:
+	string country;
+	string city;
+	string street;
+public:
+	Location(std::string new_country, std::string new_city, std::string new_street);
+	~Location();
+	string getCountry();
+	string getCity();
+	string getStreet();
+	void display();
+};
+
