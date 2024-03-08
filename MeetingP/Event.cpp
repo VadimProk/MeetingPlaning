9add07f86f5870f5965cff48985e57d90ac730b0
VadimProk <provad40@gmail.com>
2024-03-08 12:21:27 +0200

    Add static to the Event clas

--- MeetingP/Event.cpp
+++ MeetingP/Event.cpp
@@ -7,7 +7,9 @@ Event::Event(string new_name) :
 	Event{ new_name, 0 } {}
 
 Event::Event(string new_name, int new_cost) :
-	name{ new_name }, cost{ new_cost } {}
+	name{ new_name }, cost{ new_cost } {
+	amountOfEve++;
+}
 
 Event::~Event() {}
 
@@ -33,4 +35,10 @@ Event::Event(const Event& event):
 
 Event::Event(Event&& event) noexcept :
 	name{event.name}, cost{event.cost}{}
+
+int Event::amountOfEve = 0;
+int Event::getAmountOfEve()
+{
+	return amountOfEve;
+}
 
--- MeetingP/Event.h
+++ MeetingP/Event.h
@@ -7,12 +7,14 @@ class Event
 private:
 	string name;
 	int cost;
+	static int amountOfEve;
 public:
 	Event(string new_name);
 	Event(string new_name, int new_cost);
 	Event(const Event& event);
 	Event(Event&& event) noexcept;
 	~Event();
+	static int getAmountOfEve();
 	string getName();
 	int getCost();
 	void display();
