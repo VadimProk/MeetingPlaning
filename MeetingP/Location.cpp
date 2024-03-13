ad14c4b6e33a66e58703dfba55d67be94e574811
VadimProk <provad40@gmail.com>
2024-03-13 19:52:43 +0200

    Add operator overloading "=,>>,<<" to the Location clas

--- MeetingP/Location.cpp
+++ MeetingP/Location.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+Location::Location():
+	Location{"Unknown","Unknown" ,"Unknown" } {}
+
 Location::Location(std::string new_country, std::string new_city, std::string new_street) :
 	country{ new_country }, city{ new_city }, street{ new_street } {
 	amountOfLoc++;
@@ -11,22 +14,22 @@ Location::Location(std::string new_country, std::string new_city, std::string ne
 
 Location::~Location() {}
 
-string Location::getCountry()
+string Location::getCountry() const
 {
 	return country;
 }
 
-string Location::getCity()
+string Location::getCity() const
 {
 	return city;
 }
 
-string Location::getStreet()
+string Location::getStreet() const
 {
 	return street;
 }
 
-void Location::display()
+void Location::display() const
 {
 	cout << "Країна : " << country << endl;
 	cout << "Місто : " << city << endl;
@@ -44,4 +47,31 @@ int Location::amountOfLoc = 0;
 int Location::getAmountOfLoc()
 {
 	return amountOfLoc;
+}
+
+Location& Location::operator=(const Location& rhs)
+{
+	if (this == &rhs) {
+		return *this;
+	}
+	country = rhs.country;
+	city = rhs.city;
+	street = rhs.street;
+	return *this;
+}
+
+ostream& operator<<(ostream& os, const Location& obj)
+{
+	os << "Країна : " << obj.country << endl;
+	os << "Місто : " << obj.city << endl;
+	os << "Вулиця : " << obj.street << endl;
+	return os;
+}
+
+istream& operator>>(istream& is, Location& obj)
+{
+	cout << "Введіть країну - "; is >> obj.country;
+	cout << "Введіть місто - "; is >> obj.city;
+	cout << "Введіть вулицю - "; is >> obj.street;
+	return is;
 }
--- MeetingP/Location.h
+++ MeetingP/Location.h
@@ -11,15 +11,19 @@ private:
 	string street;
 	static int amountOfLoc;
 public:
+	Location();
 	Location(std::string new_country, std::string new_city, std::string new_street);
 	Location(const Location& location);
 	Location(Location&& location) noexcept;
 	~Location();
 	static int getAmountOfLoc();
-	string getCountry();
-	string getCity();
-	string getStreet();
-	void display();
+	string getCountry() const;
+	string getCity() const;
+	string getStreet() const;
+	void display() const;
+	friend ostream& operator<<(ostream& os, const Location& obj);
+	friend istream& operator>>(istream& is, Location& obj);
+	Location& operator=(const Location& rhs);
 	
 };
 
